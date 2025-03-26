e5ef206b9bf5b441b9781e671db683eedac27ecb
mmorgan12345 <64265263+mmorgan12345@users.noreply.github.com>
2025-03-26 16:49:13 -0500

    Delete Test.ps1

--- Test.ps1
+++
@@ -1,8 +0,0 @@
-$ol = New-Object -comObject Outlook.Application
-$mail = $ol.CreateItem(0)
-$mail.Subject = "Top demand apps-SOURCE CLARIFICATION"
-$mail.HTMLBody="<html><head></head><body><b>Joseph</b></body></Html>"
-$mail.save()
-
-$inspector = $mail.GetInspector
-$inspector.Display()
