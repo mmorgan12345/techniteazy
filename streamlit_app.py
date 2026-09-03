82039c15b17ec7c37e2e7db6f620e6e27fa80a61
mmorgan12345 <64265263+mmorgan12345@users.noreply.github.com>
2026-09-03 15:00:15 -0500

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+from google import genai
+
+API_KEY="AQ.Ab8RN6JwKfYEBzQcR-cBSxv8vHeyNRiVeARk1u_T3ioihu8DhA"
+
+# Automatically picks up GEMINI_API_KEY from your environment variables
+client = genai.Clilent(api_key=API_KEY)
+
+response = client.models.generate_content(
+    model="gemini-3.7-flash",
+    contents="Why is the sky blue?",
+)
+
+print(response.text)
